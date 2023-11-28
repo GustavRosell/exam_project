b2969e080defe12fbb5bac77f98d4dbd9959239a
rasmussig <rasmus2001@live.dk>
2023-11-28 08:50:21 +0100

    Sui

--- VagtplanApp/Shared/Model/Person.cs
+++ VagtplanApp/Shared/Model/Person.cs
@@ -11,13 +11,11 @@ namespace VagtplanApp.Shared.Model
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; private set; }
-
         public string Email { get; set; }
+        public int Telefonnummer { get; set; }
         public string ForNavn { get; set; }
         public string EfterNavn { get; set; }
         public DateOnly FødselsDato { get; set; }
-        public string By { get; set; }
-        public string Land { get; set; }
         public string Køn { get; set; }
         public bool isKoordinator { get; set; }
     }
