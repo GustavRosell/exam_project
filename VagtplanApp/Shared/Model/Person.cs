2e9bdbc108d6f4608d2ffc93a5c57552f15e38d8
MikkelHv <mikkelh208@gmail.com>
2023-11-28 11:03:58 +0100

    Koordinator/frivillig

--- VagtplanApp/Shared/Model/Person.cs
+++ VagtplanApp/Shared/Model/Person.cs
@@ -1,6 +1,7 @@
 ﻿
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace VagtplanApp.Shared.Model
 {
@@ -10,12 +11,18 @@ namespace VagtplanApp.Shared.Model
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 24);
-        public string Email { get; set; } 
         public int Telefonnummer { get; set; }
         public string ForNavn { get; set; }
         public string EfterNavn { get; set; }
         public DateOnly FødselsDato { get; set; }
         public string Køn { get; set; }
-        public bool isKoordinator { get; set; }
+        public bool isKoordinator { get; set; } = false;
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; } // Bemærk: Dette burde være hashet i en rigtig applikation
     }
 }
