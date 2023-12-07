e6394415a73a08f3ec4efa64e70d6f03319af0e6
rosell <Gustavrosell@gmail.com>
2023-12-07 10:27:14 +0100

    slettet minAlder + redigeret opretVagt

--- VagtplanApp/Shared/Model/Shift.cs
+++ VagtplanApp/Shared/Model/Shift.cs
@@ -12,9 +12,11 @@ namespace VagtplanApp.Shared.Model
         [BsonRepresentation(BsonType.ObjectId)]
         public string id { get; set; } = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 24);
 
+        public DateOnly date { get; set; }
+
         public DateTime startTime { get; set; }
         public DateTime endTime { get; set; }
-        public int minAge { get; set; }
+        
         public int numberOfPersons { get; set; }  // antager dette er et antal, kunne også være en liste af person-ID'er?
         public List<string> assignedPersons { get; set; } = new List<string>(); // String eller int? kommer an på id
 
